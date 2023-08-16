34a2d47c213eb0fe48cf083d218d46b98a58e72b
Fardeen Ahamed <1si20et403@sit.ac.in>
2023-08-16 20:42:32 +0530

    redux learing increamenting by 2 values and decreamenting by 2 values added

--- redux-demo.js
+++ redux-demo.js
@@ -3,8 +3,14 @@ const countReducer = (state = { counter: 0 }, action) => {
   if (action.type === "increment") {
     return { counter: state.counter + 1 };
   }
+  if (action.type === "increment2") {
+    return { counter: state.counter + 2 };
+  }
   if (action.type === "decrement") {
     return { counter: state.counter - 1 };
+  }
+  if (action.type === "decrement2") {
+    return { counter: state.counter - 2 };
   }
   return state;
 };
@@ -21,3 +27,6 @@ store.dispatch({ type: "increment" });
 store.dispatch({ type: "increment" });
 store.dispatch({ type: "decrement" });
 store.dispatch({ type: "decrement" });
+store.dispatch({ type: "increment2" });
+store.dispatch({ type: "decrement2" });
+store.dispatch({ type: "decrement2" });
